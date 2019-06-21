3248163f08f26a02080ae328f936911be3aa9e15
Nishant Singh <saysnishant@gmail.com>
2019-06-21 19:34:39 +0530

    added firebase.py template

---
+++ rasa/firebase.py
@@ -0,0 +1,16 @@
+
+
+def createNewOKR(okrBody, userEmail):
+    pass
+
+
+def updateExistingOKR(okrId, newOkrBody, userEmail):
+    pass
+
+
+def deleteExistingOKR(okrId, userEmail):
+    pass
+
+
+def listOKRs(userEmail):
+    pass
